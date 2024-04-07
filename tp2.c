4a5073f2ed3a480ccea1f29263a4a0004e0f636a
mustafa <mustafahaji210504@gmail.com>
2024-04-07 19:24:20 +0530

    more

--- tp2.c
+++ tp2.c
@@ -1,52 +1,66 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define INF 99999 
+struct Graph{
+    struct node **head;
+};
+struct node{
+    int dest;
+    struct node *next;
+};
 
-void warshall(int **table, int n){
-    for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(table[i][k]+table[k][j] < table[i][j]){
-                    table[i][j]=table[i][k]+table[k][j];
-                }
-            }
+struct Edge{
+    int src, dest;
+};
+
+struct Graph *createGraph(int V, int E, struct Edge edges[]){
+    struct Graph *graph=(struct Graph*)malloc(sizeof(struct Graph));
+    graph->head = (struct node **)malloc(V*sizeof(struct node *));
+    for(int i=0;i<V;i++){
+      graph->head[i]=NULL;
+    }
+      for (int i = 0; i < E; i++)
+    {
+        int src = edges[i].src;
+        int dest = edges[i].dest;
+        struct node *Newnode=(struct node*)malloc(sizeof(struct node));
+        Newnode->dest=dest;
+        Newnode->next=graph->head[src];
+        graph->head[src]=Newnode;
+  
+}
+
+return graph;}
+
+void printGraph(struct Graph *graph, int V)
+{
+    for (int i = 0; i < V; i++)
+    {
+        struct node *ptr = graph->head[i];
+        while (ptr != NULL)
+        {
+            printf("(%d —> %d)\t", i, ptr->dest);
+            ptr = ptr->next;
         }
+
+        printf("\n");
     }
 }
 int main(){
-    int n, e ,u, v,w,i,j;
-    printf("\nEnter no of vertices: ");
-    scanf("%d", &n);
-    int  **table=(int **)malloc(n*sizeof(int *));
-    for (i=0;i<n;i++){
-        table[i]=(int*)malloc(n*sizeof(int));
-        for(j=0;j<n;j++){
-            table[i][j]=INF;
-        }
-    }
-    printf("\nEnter no of edges: ");
-    scanf("%d",&e);
-    for (i = 0; i < e; i++) {
-    printf("Enter the end vertices of edge and weight %d: ", i + 1);
-    scanf("%d %d %d", &u, &v, &w);
-    table[u][v] = w;
-    }
-    printf("\nMatrix of input data:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++)
-            printf("%d\t", table[i][j]);
-        printf("\n");
-    }
-        warshall(table, n);
+    int V; int E;
+    printf("enter no of vertices: \n");
+    scanf("%d", &V);
+        printf("enter no of edges: \n");
+    scanf("%d", &E);
+    struct Edge edges[E];
 
-    printf("\nTransitive closure:\n");
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++)
-            printf("%d\t", table[i][j]);
-        printf("\n");
+    for (int i = 0; i < E; i++)
+    {
+        printf("enter source");
+        scanf("%d", &edges[i].src);
+        printf("enter dest");
+        scanf("%d", &edges[i].dest);
     }
-
-
-
+        struct Graph *graph = createGraph(V,E,edges);
+    printGraph(graph, V);
     return 0;
 }
